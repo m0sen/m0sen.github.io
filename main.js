89eb358299124819dc2f6c02784103fad35fa05e
Mohsen Muhammadi Shayan <m0sen@users.noreply.github.com>
2016-04-17 18:57:52 +0430

    Added files via upload

---
+++ main.js
@@ -0,0 +1,1 @@
+(function($){"use strict";$(window).load(function(){$('#page-loader').delay(300).fadeOut(400,function(){});$('#loader-name').addClass('loader-left');$('#loader-job').addClass('loader-right');$('#loader-animation').addClass('loader-hide');});$(document).ready(function(){$('#loading-wraper').fadeIn(300);});})(jQuery);
